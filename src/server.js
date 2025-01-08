021679e50195a91a4b87d31e34070b2359d9f483
Abdulrahman-Abdulhak <bdalrhmanbdalhq@gmail.com>
2025-01-08 16:53:21 +0300

    project initialized

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import { poolHostDB } from "./db/index.js";
+import { errorHandler, notFound } from "./middleware/index.js";
+
+dotenv.config();
+const app = express();
+const port = parseInt(process.env.SERVER_PORT ?? 3000);
+
+//* middlewares
+app.use(express.json());
+
+//* routes
+app.use(notFound);
+app.use(errorHandler);
+
+try {
+  const pool = await poolHostDB();
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+} catch (e) {
+  console.error(e);
+}
